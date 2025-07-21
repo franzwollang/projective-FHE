205b7c31cd04a0861805f34ab26f94e59dc52a8c
Franz Wollang <franzwollang@gmail.com>
2025-07-21 17:14:14 -0500

    Clean CMake cache files and add comprehensive .gitignore

--- openfhe_prototype/build/CMakeFiles/benchmark_modes.dir/compiler_depend.make
+++
@@ -1,2 +0,0 @@
-# Empty compiler generated dependencies file for benchmark_modes.
-# This may be replaced when dependencies are built.
--- openfhe_prototype/build/CMakeFiles/fhe_pipeline.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/fhe_pipeline.dir/pipeline.cpp.o"
-  "CMakeFiles/fhe_pipeline.dir/pipeline.cpp.o.d"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang CXX)
-  include(CMakeFiles/fhe_pipeline.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
